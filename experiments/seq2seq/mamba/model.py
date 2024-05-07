e7611c6ebc1365cc98c3713b31cfe1cfb9c2d882
Maximilian.Burzer <maxi.burzer@gmail.com>
2024-05-07 13:10:41 +0200

    continue

--- experiments/seq2seq/mamba/model.py
+++ experiments/seq2seq/mamba/model.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Union
 import torch
 import torch.nn as nn
 from positional_encodings.torch_encodings import PositionalEncodingPermute2D, Summer  # type: ignore
@@ -9,8 +9,6 @@ from transformers import PreTrainedTokenizerFast  # type: ignore
 class Encoder(nn.Module):
     def __init__(self, hidden_size: int) -> None:
         super(Encoder, self).__init__()
-
-        self.pos_enc_2d_summer = Summer(PositionalEncodingPermute2D(hidden_size))
 
         self.sequential = nn.Sequential(
             nn.Conv2d(1, 64, 3, padding=1),
@@ -34,10 +32,8 @@ class Encoder(nn.Module):
         )
 
     def forward(self, x: Tensor) -> Tensor:
-        x = self.sequential(x)
-        x = self.pos_enc_2d_summer(x)
-        x = x.view(x.shape[0], x.shape[1], -1)
-        return x
+        feature_maps: Tensor = self.sequential(x)
+        return feature_maps
 
 
 class Decoder(nn.Module):
@@ -45,14 +41,15 @@ class Decoder(nn.Module):
         self,
         embed_size: int,
         hidden_size: int,
-        num_layers: int,
         tokenizer: PreTrainedTokenizerFast,
+        device: torch.device = torch.device("cpu"),
     ) -> None:
         super(Decoder, self).__init__()
 
+        self.device = device
         self.tokenizer = tokenizer
         self.embed = nn.Embedding(tokenizer.vocab_size, embed_size)
-        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(embed_size, hidden_size, batch_first=True)
         self.linear = nn.Linear(hidden_size, tokenizer.vocab_size)
         self.dropout = nn.Dropout(0.5)
 
@@ -61,42 +58,53 @@ class Decoder(nn.Module):
     ) -> Tuple[Tensor, Tuple[Tensor, Tensor]]:
         embedding = self.embed(input_token_id)
         embedding = self.dropout(embedding)
-        print(embedding.shape)
 
+        print(states[0].shape)
+        print(embedding.shape)
         output, states = self.lstm(embedding, states)
         logits = self.linear(output)
         print(logits.shape)
 
-        id = logits.argmax(1)
-        return id, states
+        return logits, states
 
     def forward(
         self,
-        features: Tensor,
-        target_token_ids: Tensor | None = None,
+        states: Tuple[Tensor, Tensor],
+        target_token_ids: Union[Tensor, None] = None,
         max_len: int = 50,
-    ) -> str:
-        batch_size = features.shape[0]
-        input_token_id = torch.empty(batch_size, 1).fill_(
-            self.tokenizer.token_to_id("[SOS]")
+    ) -> Tensor:
+        batch_size = states[0].shape[0]
+        input_token_id = (
+            torch.empty(batch_size, 1, dtype=torch.int64)
+            .fill_(self.tokenizer.convert_tokens_to_ids("[BOS]"))
+            .to(self.device)
         )
-        states = (features.mean(2), features.mean(2))
-        output_token_ids = []
+        print(input_token_id.shape)
+
+        logits_list = []
 
         for i in range(max_len):
-            output_token_id, states = self.forward_step(input_token_id, states)
-            output_token_ids.append(output_token_id)
+            # shape: (1, batch_size, hidden_size)
+            states = states[0].unsqueeze(0), states[1].unsqueeze(0)
+
+            logits, states = self.forward_step(input_token_id, states)
+
+            logits_list.append(logits)
+            output_token_id = logits.argmax(1)
 
             if target_token_ids is not None:
-                input_token_id = target_token_ids[:, i].unsqueeze(1)
+                input_token_id = target_token_ids[:, i]
             else:
                 input_token_id = output_token_id.detach()
 
-            if self.tokenizer.id_to_token(output_token_id) == "[EOS]":
+            ids = output_token_id.tolist()
+            print(ids)
+            tokens = self.tokenizer.convert_ids_to_tokens()
+
+            if all(token == "[EOS]" for token in tokens):
                 break
 
-        equation = str(self.tokenizer.decode(output_token_ids))
-        return equation
+        return torch.stack(tuple(logits_list))
 
 
 class EncoderDecoder(nn.Module):
@@ -104,21 +112,28 @@ class EncoderDecoder(nn.Module):
         self,
         embed_size: int,
         hidden_size: int,
-        num_layers: int,
         tokenizer: PreTrainedTokenizerFast,
+        device: torch.device = torch.device("cpu"),
     ) -> None:
         super(EncoderDecoder, self).__init__()
 
+        self.device = device
         self.tokenizer = tokenizer
         self.encoder = Encoder(hidden_size)
-        self.decoder = Decoder(embed_size, hidden_size, num_layers, tokenizer)
+        self.pos_enc_2d_summer = Summer(PositionalEncodingPermute2D(hidden_size))
+        self.decoder = Decoder(embed_size, hidden_size, tokenizer, device)
 
     def forward(
         self,
         img: Tensor,
-        target_token_ids: Tensor | None = None,
+        target_token_ids: Union[Tensor, None] = None,
         max_len: int = 50,
-    ) -> str:
-        features = self.encoder(img)
-        equation = self.decoder(features, target_token_ids, max_len)
-        return str(equation)
+    ) -> Tensor:
+        feature_maps = self.encoder(img)
+        feature_maps = self.pos_enc_2d_summer(feature_maps)
+
+        features = feature_maps.view(feature_maps.shape[0], feature_maps.shape[1], -1)
+        states = (features.mean(2), features.mean(2))
+
+        logits_tensor: Tensor = self.decoder(states, target_token_ids, max_len)
+        return logits_tensor
